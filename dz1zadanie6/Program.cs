1d9c2744229832c29a24967aa2a9441a7303733e
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-06 17:56:24 +0300

    задача6

---
+++ dz1zadanie6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 1) 
+{
+    Console.WriteLine($"Число {n} нечетное");
+}
+ else
+{
+    Console.WriteLine($"Число {n} четное");
+}
