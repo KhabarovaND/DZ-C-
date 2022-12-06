bf7d9c2d55b354dd88f43df35120425ad4b63106
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-06 16:56:38 +0300

    задача2

---
+++ dz1zadanie2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите первое целое число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе целое число");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1>n2)
+{
+Console.WriteLine($"число {n1} больше числа {n2}");
+}
+else
+{
+Console.WriteLine($"число {n2} больше числа {n1}");
+}
