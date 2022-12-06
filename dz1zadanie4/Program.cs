d7fe4883b35bd659f29b74b3f6b3071579ca1888
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-06 17:25:38 +0300

    задача4

---
+++ dz1zadanie4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2)
+{
+    if (n1 > n3)
+    {
+        Console.WriteLine("Максимальное число: " + n1);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + n3);
+    }
+}
+
+else if (n2 > n3)
+{
+    Console.WriteLine("Максимальное число: " + n2);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + n3);
+}
