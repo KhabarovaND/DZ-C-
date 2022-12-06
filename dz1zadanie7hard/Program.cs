c30d03afd6947d41b399462d0a1938c643e7b183
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-06 18:30:06 +0300

    задача7hard

---
+++ dz1zadanie7hard/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int a=0, b=0;
+if (num>99)
+{
+    b = num;
+    while (b>=1000)
+    {
+        b/=10;
+        a=b%10;
+    }
+    Console.WriteLine($"{a} - третья цифра");
+}
+else
+{
+  Console.WriteLine("третья цифра отсутствует");  
+}
