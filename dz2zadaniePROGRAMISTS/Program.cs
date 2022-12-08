2e3ef460148d6ad52142430de5499c6d03d39341
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-08 19:20:14 +0300

    задачаПРОГРАММИСТЫ

---
+++ dz2zadaniePROGRAMISTS/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите колличество программистов");
+int n = Convert.ToInt32(Console.ReadLine());
+    if (n % 100>=10 & n % 100 <=20 )
+    {
+        Console.WriteLine($"{n} программистОВ");
+    }
+    else if (n % 10 == 1)
+    {
+        Console.WriteLine($"{n} программист");
+    }
+    else if (n % 10 >= 2 & n % 10 <= 4)
+    {
+        Console.WriteLine($"{n} программистА");
+    }
+    else
+    {
+        Console.WriteLine($"{n} программистОВ");
+    }
