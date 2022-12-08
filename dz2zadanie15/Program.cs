df58cdeb7a0b5970564e396e899a9d1476d324bf
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-08 17:27:14 +0300

    задача15

---
+++ dz2zadanie15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число, обозначающее день недели");
+int n = Convert.ToInt32(Console.ReadLine());
+ if (n<6)
+{
+    Console.WriteLine("Рабочий день");
+}
+else 
+{
+Console.WriteLine("Выходной");
+}
