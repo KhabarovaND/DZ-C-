b2a94536f42c9de82ca3140bd078248c672c4eb5
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-08 16:55:36 +0300

    задача10

---
+++ dz2zadanie10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int a=0, b=0;
+if (num>99)
+{
+    b = num;
+    while (b>=100)
+    {
+        b/=10;
+        a=b%10;
+    }
+    Console.WriteLine($"{a} - вторая цифра");
+}
+else
+{
+  Console.WriteLine("вторая цифра отсутствует");  
+}
