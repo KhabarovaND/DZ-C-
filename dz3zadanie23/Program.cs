e218c5f87e97e3cf33704faea768f4a939433bbc
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-09 19:10:36 +0300

    задача23

---
+++ dz3zadanie23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+try
+{
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=n;i++)
+{
+Console.Write($"{i*i*i} " );
+}
+Console.WriteLine();
+}
+catch
+{
+    Console.WriteLine("некорректный ввод данных");
+}
