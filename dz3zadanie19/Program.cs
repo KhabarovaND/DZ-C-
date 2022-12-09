63a0fd328dcf071c3e82209c19c0af781c8c5351
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-09 18:39:36 +0300

    задача19

---
+++ dz3zadanie19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+string n = Console.ReadLine();
+int len = n.Length;
+
+if (len == 5)
+{
+    if (n[0] == n[4] && n[1] == n[3])
+    {
+        Console.WriteLine($"{n} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{n} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($" {n} - не является пятизначным");
+}
