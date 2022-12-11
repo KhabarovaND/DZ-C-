9a76c81d1e64e3469527f72897b4c3ff07797137
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-11 17:39:11 +0300

    задача25

---
+++ dz4zadanie25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число A");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+double b = Convert.ToInt32(Console.ReadLine());
+double n = a;
+
+for (double i = 1; i < b; i++)
+{
+n = n * a;
+}
+Console.WriteLine($"{a} в степени {b} равно: {n}");
