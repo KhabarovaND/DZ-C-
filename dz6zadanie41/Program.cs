5ec8f5d93a939b5ac84eb6a890c02c93a0c8ee8c
Nadezhda <N.D.Khabarova@gmail.com>
2022-12-15 19:02:19 +0300

    задача41

---
+++ dz6zadanie41/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Задайте размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine()); // Читаем с клавиатуры размер массива и записываем в size
+	
+int[] array = new int[size]; // Создаём массив int размером в size
+System.Console.WriteLine("Введите числа:");
+/*пишем числа массив*/
+for (int i = 0; i < size; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine()); ; // Заполняем массив элементами, введёнными с клавиатуры
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++) //проверяем каждое число на положительность)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
