2eb4b546d0d25f7bdb00c4e4878f42d23e3fb7a1
Nadezhda <N.D.Khabarova@gmail.com>
2023-01-08 17:20:31 +0300

    задача66

---
+++ dz9zadanie66/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+GapNumberSum(M, N, 0);
+
+///Метод нахождения суммы натуральных элементов в промежутке от M до N
+void GapNumberSum (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    GapNumberSum(M, N, sum);
+}
+
